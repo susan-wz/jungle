8ccfb058808cfac36350207ccdbf6f89db15c5df
Susan Zhang <susanwenzhang@gmail.com>
2020-02-11 21:29:34 +0000

    added ability to edit products

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'dashboard#show'
-    resources :products, except: [:edit, :update, :show]
+    resources :products, except: [:show]
     resources :categories, only: [:index, :new, :create]
     resources :sales, only: [:index, :new, :create, :destroy]
   end
