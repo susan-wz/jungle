7fce1d491e9281d479584dbabaa8267d6a117c9b
Susan <susanwenzhang@gmail.com>
2020-03-03 19:02:45 -0500

    added and changed fonts, before removing bootstrap nav

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,9 @@ module Jungle
       g.assets       false
     end
 
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
