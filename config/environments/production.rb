120da0d87adf4b8168e858b8456d44839d0d4d57
Susan <susanwenzhang@gmail.com>
2020-03-03 22:19:16 -0500

    trying to fix deployment issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  config.secret_token = ENV["SECRET_TOKEN"]
+
 end
